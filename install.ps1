24ef95331e3272921711a3d24efd47b352a416f3
ronwellman <ron.wellman01@gmail.com>
2020-02-20 15:19:43 -0500

    Upload module installation script.

---
+++ install.ps1
@@ -0,0 +1,15 @@
+Start-Transcript C:\module_installation.txt
+
+Write-Output "Module Path"
+$Env:PSModulePath
+
+Write-Output "Installing PackageProvider"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Write-Output "Setting PSRepository"
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+Write-Output "Installing Modules"
+Install-Module xActiveDirectory, xComputerManagement, xNetworking, xAdcsDeployment, xDnsServer, xSystemSecurity -confirm:$false -Verbose -Scope AllUsers -Force
+
+Stop-Transcript
