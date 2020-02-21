5bce7b2bbda33136243b48f273060485f764e00e
ronwellman <ron.wellman01@gmail.com>
2020-02-21 08:43:32 -0500

    Remove verbosity setting.

--- install.ps1
+++ install.ps1
@@ -10,6 +10,6 @@ Write-Output "Setting PSRepository"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Write-Output "Installing Modules"
-Install-Module xActiveDirectory, xComputerManagement, xNetworking, xAdcsDeployment, xDnsServer, xSystemSecurity -confirm:$false -Verbose -Scope AllUsers -Force
+Install-Module xActiveDirectory, xComputerManagement, xNetworking, xAdcsDeployment, xDnsServer, xSystemSecurity -confirm:$false -Scope AllUsers -Force
 
 Stop-Transcript
